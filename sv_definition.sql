fb6a0429208383f3563c67a12634cc2609e86270
j-dover <j-dover@users.noreply.github.com>
2018-05-31 01:21:03 -0400

    Add title and location attributes to opportunities

--- sv_definition.sql
+++ sv_definition.sql
@@ -55,6 +55,8 @@ CREATE TABLE professionals (
 CREATE TABLE opportunities (
     id int(11) NOT NULL AUTO_INCREMENT,
     pid int(11) NOT NULL,
+    title varchar(255) NOT NULL,
+    location varchar(255) NOT NULL,
     description text NOT NULL,
     postingDate date NOT NULL,
     industry varchar(255) NOT NULL,
