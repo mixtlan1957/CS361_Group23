871f8e5bc0cd49a004f6802545d7d30ea4e4ab1d
Sean Foster <seankfost@gmail.com>
2018-05-31 19:48:03 -0600

    add user query example

--- queries/account.js
+++ queries/account.js
@@ -1,5 +1,26 @@
 var methods = {
   
+  addUser: function(user, res, mysql, context, complete){
+    var sqlQuery = "INSERT INTO users (username, password, email)";
+    var inserts = [user.username, user.password, user.email];
+    mysql.pool.query(sqlQuery, inserts,
+      function(error, results, fields){
+        if(error){
+          console.log(error);
+          return;
+        }
+        complete();
+      });
+  },
+
+  addStudent: function(user, res, mysql, context, complete){
+    complete();
+  },
+
+  addProfessional: function(user, res, mysql, context, complete){
+    
+  }
+
 };
 
 exports.data = methods;
