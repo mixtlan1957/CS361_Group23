8f6847181f3993ecfb868c002d6b84bc7800d5c5
j-dover <j-dover@users.noreply.github.com>
2018-06-03 23:28:46 -0400

    search implementation for database

---
+++ queries/search.js
@@ -0,0 +1,13 @@
+var methods = {
+    search: function(sql, res, mysql, context, complete){
+        mysql.poolquery(sql, function(error, results, fields){
+            if(error)
+            {
+                console.log(error);
+                res.end();
+            }
+            context.opportunities = results;
+            complete();
+        });
+    }
+};
