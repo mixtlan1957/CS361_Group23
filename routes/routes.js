d5bb62b5475d7ca8d895124f026bd813bb0c74c8
Anmolbir <anmolbirm@mrcodeswildride.com>
2018-06-06 01:03:17 +0000

    :Added professional unverified signup into database

--- routes/routes.js
+++ routes/routes.js
@@ -11,45 +11,42 @@ var mysql; // mysql module to use
 /*
  * imports MYSQL module to use
  */
-function setMySQL(input)
-{
+function setMySQL(input) {
   mysql = input;
   sv_db.setMySQL(mysql); // connect mysql to the sv_database 
 }
 
 /******************************************************************************
  * GET /home ; renders home view
-******************************************************************************/
-router.get('/', function(req,res,next){
+ ******************************************************************************/
+router.get('/', function(req, res, next) {
   var context = {};
-  if(req.session.username)
-  {
-    context.username = req.session.username;  
+  if (req.session.username) {
+    context.username = req.session.username;
   }
   res.render('home', context);
 });
 
 /******************************************************************************
  * GET /search ; renders search view
-******************************************************************************/
-router.get('/search', function(req, res, next){
-    var context = {};
-    if(req.session.username){
-        context.username = req.session.username;
-    }
-    context.title = "Search";
-    res.render('search', context);
+ ******************************************************************************/
+router.get('/search', function(req, res, next) {
+  var context = {};
+  if (req.session.username) {
+    context.username = req.session.username;
+  }
+  context.title = "Search";
+  res.render('search', context);
 });
 
 
 /******************************************************************************
  * GET /about ; renders about view
-******************************************************************************/
-router.get('/about', function(req,res,next){
+ ******************************************************************************/
+router.get('/about', function(req, res, next) {
   var context = {};
-  if(req.session.username)
-  {
-    context.username = req.session.username;  
+  if (req.session.username) {
+    context.username = req.session.username;
   }
   context.title = "About Us";
   res.render('about', context);
@@ -57,108 +54,131 @@ router.get('/about', function(req,res,next){
 
 /******************************************************************************
  * GET /signup ; renders signup view
-******************************************************************************/
-router.get('/signup', function(req,res,next){
+ ******************************************************************************/
+router.get('/signup', function(req, res, next) {
   var context = {};
-  if(req.session.username)
-  {
-    context.username = req.session.username;  
+  context.signup_error = req.query.signup_error;
+  if (req.session.username) {
+    context.username = req.session.username;
   }
   context.title = "Signup";
   context.jsscripts = ["signup.js"];
   res.render('accounts/signup', context);
 });
 
-router.post('/search/search_results', function(req, res){
-    var context = {};
-    var mysql = req.app.get('mysql');
-    var sql = "SELECT title FROM opportunities";
-    var title = req.body['title'];
-    var loc = req.body['location'];
+router.post('/search/search_results', function(req, res) {
+  var context = {};
+  var mysql = req.app.get('mysql');
+  var sql = "SELECT title FROM opportunities";
+  var title = req.body['title'];
+  var loc = req.body['location'];
 
-    if(title !== "" && loc !== ""){
-        sql = sql + " WHERE title =  "  + title + " AND WHERE location  =  "  + loc;
-    }
-    else if(title !== ""){
-        sql = sql + " WHERE title =  " + title;
-    }
-    else if (loc !== ""){
-        sql = sql + " WHERE location =  " + loc;
-    }
+  if (title !== "" && loc !== "") {
+    sql = sql + " WHERE title =  " + title + " AND WHERE location  =  " + loc;
+  }
+  else if (title !== "") {
+    sql = sql + " WHERE title =  " + title;
+  }
+  else if (loc !== "") {
+    sql = sql + " WHERE location =  " + loc;
+  }
 
-    context.jsscripts = ["search.js"];
-    res.render('search_results', context);
+  context.jsscripts = ["search.js"];
+  res.render('search_results', context);
 });
 
 
 /******************************************************************************
- * >> NOT WORKING << POST /signup/student ; rebuilds user object from the form
-                 getUserId is returning undefinied for some reason... otherwise this should work.                          
-******************************************************************************/
-router.post('/signup/student', function(req, res){
-  var context = {};
-  context.jsscripts = ["profile.js"];
-
-  req.session.username = req.body.username;
-  context.username = req.session.username;
-
-
-  req.session.email = req.body.email;
-  context.email = req.session.email;
-
-  req.body.password = parseInt(req.body.password);
-  if(isNaN(req.body.password)){
-    req.body.password = 0;
-  }
-  req.session.fname = req.body.fname;
-  context.fname = req.session.fname;
-
-  req.session.lname = req.body.lname;
-  context.lname = req.session.lname;
-  
-  req.session.age = req.body.age;
-  context.age = req.session.age;
-
-  req.session.grade = req.body.grade;
-  context.grade = req.session.grade;
-
-  req.session.sname = req.body.sname;
-  context.sname = req.session.sname;
-
-
-
-  if(!req.session.username){
-    res.render('accounts/signup', context);
-    return;
-  }
+ * >> Handles request to create new student
+ ******************************************************************************/
+router.post('/signup/student', function(req, res) {
 
   var input = [req.body.username, req.body.password, req.body.email,
-               req.body.fname, req.body.lname, req.body.sname, req.body.age, req.body.grade];
+    req.body.fname, req.body.lname, req.body.sname, req.body.age, req.body.grade
+  ];
+
   sv_db.callProcedure("newStudent", input, complete);
 
-  function complete(rows, err){
-   if(err){
+  // callback database procedure
+  function complete(rows, err) {
+
+    if (err) // if an error occured
+    {
       console.log(err);
-      rows = [];
-   }
-    console.log(rows);
-    res.render('accounts/student/studentprofile', context); 
-  } 
+      res.redirect('/signup?signup_error=true');
+      return;
+    }
+    var context = {};
+    context.jsscripts = ["profile.js"];
+    req.session.username = req.body.username;
+    context.username = req.session.username;
+
+
+    req.session.email = req.body.email;
+    context.email = req.session.email;
+
+    req.body.password = parseInt(req.body.password);
+    if (isNaN(req.body.password)) {
+      req.body.password = 0;
+    }
+    req.session.fname = req.body.fname;
+    context.fname = req.session.fname;
+
+    req.session.lname = req.body.lname;
+    context.lname = req.session.lname;
+
+    req.session.age = req.body.age;
+    context.age = req.session.age;
+
+    req.session.grade = req.body.grade;
+    context.grade = req.session.grade;
+
+    req.session.sname = req.body.sname;
+    context.sname = req.session.sname;
+    res.render('accounts/student/studentprofile', context);
+  }
 
 });
 
+/******************************************************************************
+ * POST- creates new professional request
+ ******************************************************************************/
 router.post('/signup/professionalSubmit', function(req, res) {
+  var input = [
+    req.body.username, req.body.password, req.body.email,
+    req.body.fname, req.body.lname, req.body.cname, 0
+  ];
+  sv_db.callProcedure("newProfessional", input, complete);
+
+  // callback database procedure
+  function complete(rows, err) {
+    if (err) // if an error occured
+    {
+      console.log(err);
+      res.redirect('/signup?signup_error=true');
+      return;
+    }
+    var context = {};
+    context.pendingVerification = true;
+    res.render('home', context);
+  }
+  var noEmail = true;
+  // don't email for testing
+  if (noEmail) {
+    return;
+  }
+
   var submitterEmail = "<" + req.body.email + ">";
   var submitterName = req.body.fname + " " + req.body.lname;
   var subIndustry = req.body.industry;
   var subJobTitle = req.body.jobTitle;
   var statement = req.body.statement;
-// Generate SMTP service account from ethereal.email
-nodemailer.createTestAccount((err, account) => {   //citation/source: https://nodemailer.com/about/
+  // Generate SMTP service account from ethereal.email
+  nodemailer.createTestAccount((err, account) => { //citation/source: https://nodemailer.com/about/
     if (err) {
-        console.error('Failed to create a testing account');
-        console.error(err);
-        return process.exit(1);
+      console.error('Failed to create a testing account');
+      console.error(err);
+      return process.exit(1);
     }
 
     console.log('Credentials obtained, sending message...');
@@ -187,14 +207,14 @@ nodemailer.createTestAccount((err, account) => {   //citation/source: https://no
             //from: 'Pangalink <no-reply@pangalink.net>',
             from: submitterName + ' ' + submitterEmail,  
             headers: {
-                'X-Laziness-level': 1000 // just an example header, no need to use this
+                'X-Laziness-level': 1000 // just an example header, not neccessary
         }
     });
     */
     // this chunk of code sets the transporter to an actual gmail address
     var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
+      service: 'gmail',
+      auth: {
         user: 'cs361.groupproj@gmail.com',
         pass: 'projectonly!'
       }
@@ -202,55 +222,59 @@ nodemailer.createTestAccount((err, account) => {   //citation/source: https://no
 
     // Message object
     let message = {
-        // Comma separated list of recipients
-        to: 'Mario <mariofm.mx@gmail.com>, Jennifer <aldoverj@oregonstate.edu>,' + 
-        'Sean <fostesea@oregonstate.edu>, Anmolbir <mannanm@oregonstate.edu>, Andrew <schappea@oregonstate.edu>',
+      // Comma separated list of recipients
+      to: 'Mario <mariofm.mx@gmail.com>, Jennifer <aldoverj@oregonstate.edu>,' +
+        'Sean <fostesea@oregonstate.edu>, Anmolbir <mannanm@oregonstate.edu>,' +
+        ' Andrew <schappea@oregonstate.edu>',
 
-        // Subject of the message
-        subject: 'Professional Account Applicant: ' + submitterName,
+      // Subject of the message
+      subject: 'Professional Account Applicant: ' + submitterName,
 
-        // plaintext body
-        text: 'Submitter Name: ' + submitterName + '\nIndustry: ' + subIndustry + '\nJob Title/Role:' + subJobTitle + 
-        '\nContact Email Listed: ' + submitterEmail + '\n\nStatement of Intent: ' + statement
+      // plaintext body
+      text: 'Submitter Name: ' + submitterName + '\nIndustry: ' + subIndustry +
+        '\nJob Title/Role:' + subJobTitle + '\nContact Email Listed: ' +
+        submitterEmail + '\n\nStatement of Intent: ' + statement
     };
+
 
     transporter.sendMail(message, (error, info) => {
 
-        if (error) {
-            console.log('Error occurred');
-            console.log(error.message);
-            return process.exit(1);
-        }
+      if (error) {
+        console.log('Error occurred');
+        console.log(error.message);
+        return process.exit(1);
+      }
 
-        console.log('Message sent successfully!');
-        console.log(nodemailer.getTestMessageUrl(info));
+      console.log('Message sent successfully!');
+      console.log(nodemailer.getTestMessageUrl(info));
 
-        //upon successful submission redirect user (this should probably be updated to a "success" page)
-        res.render('home', context);
+      //upon successful submission redirect user 
+      // (this should probably be updated to a "success" page)
+      res.render('home', context);
 
-        // only needed when using pooled connections
-        transporter.close();
+      // only needed when using pooled connections
+      transporter.close();
     });
+  });
 });
-});
 
-
-
-
-router.get('/login', function(req,res,next){
+/******************************************************************************
+ * GET login page
+ ******************************************************************************/
+router.get('/login', function(req, res, next) {
   var context = {};
-  if(req.session.username)
-  {
-    context.username = req.session.username;  
+  if (req.session.username) {
+    context.username = req.session.username;
   }
   context.title = "Login";
   res.render('accounts/login', context);
 });
 
-router.post('/login', function(req, res, next){
+router.post('/login', function(req, res, next) {
   var context = {};
   var input = [req.body.username, req.body.password];
   sv_db.callProcedure("getUser", input, complete);
+
   function complete(rows, err) // callback after checking username and password
   {
     if (err) // error handling
@@ -271,9 +295,12 @@ router.post('/login', function(req, res, next){
   }
 });
 
-router.get('/logout', function(req, res, next){
+/******************************************************************************
+ * GET /logout logout page
+ ******************************************************************************/
+router.get('/logout', function(req, res, next) {
   var context = {};
-  if(req.session.username){
+  if (req.session.username) {
     req.session.username = null;
     context.username = req.session.username;
   }
@@ -281,27 +308,31 @@ router.get('/logout', function(req, res, next){
 });
 
 
-router.get('/profile', function(req, res, rnext){
+/******************************************************************************
+ * GET /profile page
+ ******************************************************************************/
+router.get('/profile', function(req, res, rnext) {
   var context = {};
   context.jsscripts = ["profile.js"];
-  if(req.session.username){
+  if (req.session.username) {
     context.username = req.session.username;
     context.fname = req.session.fname;
     context.lname = req.session.lname;
     context.email = req.session.email;
-    if(req.session.sname){
+    if (req.session.sname) {
       context.sname = req.session.sname;
       context.age = req.session.age;
       context.grade = req.session.grade;
       res.render('accounts/student/studentprofile', context);
       return;
-    } else if(req.session.cname){
+    }
+    else if (req.session.cname) {
       context.cname = req.session.cname;
       res.render('accounts/profile', context);
       return;
     }
   }
-  else{
+  else {
     res.render('home');
   }
 });
